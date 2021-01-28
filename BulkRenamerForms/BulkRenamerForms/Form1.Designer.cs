e8ad65fe68948c5c8f7da607364c4cca581082fb
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-01-28 18:53:49 +0000

    Init

---
+++ BulkRenamerForms/BulkRenamerForms/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace BulkRenamerForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loc = new System.Windows.Forms.TextBox();
+            this.bname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ftype = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rename = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(13, 202);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 1;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(94, 202);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 183);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Items";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ftype);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.bname);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.loc);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox2.Location = new System.Drawing.Point(217, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(198, 183);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Options";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(7, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rename Location:";
+            // 
+            // loc
+            // 
+            this.loc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.loc.Location = new System.Drawing.Point(7, 50);
+            this.loc.Name = "loc";
+            this.loc.Size = new System.Drawing.Size(185, 23);
+            this.loc.TabIndex = 1;
+            // 
+            // bname
+            // 
+            this.bname.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bname.Location = new System.Drawing.Point(6, 100);
+            this.bname.Name = "bname";
+            this.bname.Size = new System.Drawing.Size(185, 23);
+            this.bname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(6, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Base Name:";
+            // 
+            // ftype
+            // 
+            this.ftype.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ftype.Location = new System.Drawing.Point(7, 150);
+            this.ftype.Name = "ftype";
+            this.ftype.Size = new System.Drawing.Size(185, 23);
+            this.ftype.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(7, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "File Type:";
+            // 
+            // rename
+            // 
+            this.rename.Location = new System.Drawing.Point(217, 202);
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(75, 23);
+            this.rename.TabIndex = 3;
+            this.rename.Text = "Rename All";
+            this.rename.UseVisualStyleBackColor = true;
+            this.rename.Click += new System.EventHandler(this.rename_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(186, 147);
+            this.listBox1.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox3.Location = new System.Drawing.Point(421, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(198, 183);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Renamed Items";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 26);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(186, 147);
+            this.listBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 242);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.rename);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(646, 281);
+            this.MinimumSize = new System.Drawing.Size(646, 281);
+            this.Name = "Form1";
+            this.Text = "Bulk Renamer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ftype;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox bname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox loc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rename;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ BulkRenamerForms/BulkRenamerForms/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace BulkRenamerForms
+{
+    public partial class Form1 : Form
+    {
+        List<string> Files = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rename_Click(object sender, EventArgs e)
+        {
+            string Folder = "";
+            string BaseName = "";
+            string FileType = "";
+
+            if (loc.Text.Trim() != "" && bname.Text.Trim() != "" && ftype.Text.Trim() != "" && Files.Count > 0)
+            {
+                Folder = loc.Text.Trim();
+                BaseName = bname.Text.Trim();
+                FileType = ftype.Text.Trim();
+
+                for (int i = 0; i < Files.Count; i++)
+                {
+                    string Source = Files[i];
+
+                    FileInfo FileInf = new FileInfo(Source);
+
+                    string Number;
+
+                    if (i + 1 > 0 && i + 1 <= 9)
+                    {
+                        Number = "00" + (i + 1);
+                    }
+                    else
+                    {
+                        Number = "0" + (i + 1);
+                    }
+
+                    if (FileInf.Exists)
+                    {
+                        FileInf.MoveTo(Folder + BaseName + Number + "." + FileType);
+                        listBox2.Items.Add(FileInf.DirectoryName + @"\" + FileInf.Name);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("All fields MUST have a value");
+            }
+            
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Multiselect = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string s in ofd.FileNames)
+                    {
+                        Files.Add(s);
+                        listBox1.Items.Add(s);
+                    }
+                }
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            Files.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+    }
+}
